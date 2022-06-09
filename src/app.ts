ffb1fe645a67804751a54834a8cad851c4beaafd
HenriqueSerres <henriqueserres@gmail.com>
2022-06-09 15:14:00 -0300

    feat: estruturas

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import blogRoutes from './routes/userRouter';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/blogs', blogRoutes);
 
 export default app;
