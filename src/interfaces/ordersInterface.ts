fc53d04f8bb9c898a39f0ac516b9062261d63172
HenriqueSerres <henriqueserres@gmail.com>
2022-06-10 16:12:48 -0300

    req 3fix again

---
+++ src/interfaces/ordersInterface.ts
@@ -0,0 +1,5 @@
+export default interface IntOrder {
+  id: number;
+  userId: number;
+  productsIds: number[]
+}
--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -4,13 +4,15 @@ import { IntUser } from '../interfaces/usersInterface';
 
 dotenv.config();
 
+const minhaSenha = process.env.JWT_SECRET || 'suaSenhaSecreta';
+
 const jwtConfig: object = {
   expiresIn: '10d',
   algorithm: 'HS256',
 };
 
 const generateJWT = (payload: IntUser) => {
-  const token = jwt.sign({ data: payload }, JSON.stringify(process.env.JWT_SECRET), jwtConfig);
+  const token = jwt.sign({ data: payload }, minhaSenha, jwtConfig);
 
   return token;
 };
